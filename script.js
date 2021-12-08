a1bfe27e7fd45f1809b15453f81b1f2508fec510
John Bouchard <jbouchard13@gmail.com>
2021-12-07 22:28:42 -0500

    rainbow button allows random colors to be added into the etch a sketch

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const clearBtnEl = document.querySelector(".clear-btn");
 const squareCountEl = document.querySelector("#boxSelect");
 const createBtnEl = document.querySelector("#createBtn");
 const modalContainerEl = document.querySelector(".modal-container");
+const rainbowBtnEl = document.querySelector(".color-btn");
+const shaderBtnEl = document.querySelector(".shader-btn");
 let squareCount = squareCountEl.value * squareCountEl.value;
 
+let colorArray = ["red", "orange", "yellow", "green", "cyan", "blue", "violet"];
+
 let divArray = [];
+let opacity = 0.1;
+let rainbowActive = false;
+let shaderActive = false;
 
 const closeModal = () => {
   modalContainerEl.classList.add("hide-modal");
@@ -27,7 +34,18 @@ const createGrid = (squareCount) => {
     // add hover listener to each div
     // changes background to black when hovered over
     div.addEventListener("mouseover", (e) => {
-      e.target.classList.add("hover");
+      // determine how to color the tiles depending on which button is selected
+      if (rainbowActive === false) {
+        e.target.style.background = "black";
+      } else if (rainbowActive === true) {
+        e.target.style.background =
+          colorArray[Math.floor(Math.random() * colorArray.length)];
+      }
+      if (shaderActive === true) {
+        let opacity = e.target.style.opacity;
+        e.target.style.opacity = e.target.style.opacity;
+        console.log(e.target.style.opacity);
+      }
     });
     // push each div to an array to be used when clearing
     divArray.push(div);
@@ -40,6 +58,7 @@ const handleClear = () => {
   // remove the hover class from each div
   divArray.forEach((div) => {
     div.classList.remove("hover");
+    div.style.background = "white";
   });
 };
 
@@ -65,6 +84,30 @@ createBtnEl.addEventListener("click", () => {
   // clear the old grid before creating a new one
   clearOldGrid();
   createGrid(squareCount);
+});
+
+rainbowBtnEl.addEventListener("click", () => {
+  if (rainbowActive === false) {
+    rainbowActive = true;
+    rainbowBtnEl.classList.add("held");
+    return true;
+  } else if (rainbowActive === true) {
+    rainbowActive = false;
+    rainbowBtnEl.classList.remove("held");
+    return false;
+  }
+});
+
+shaderBtnEl.addEventListener("click", () => {
+  if (shaderActive === false) {
+    shaderActive = true;
+    shaderBtnEl.classList.add("held");
+    return true;
+  } else if (shaderActive === true) {
+    shaderActive = false;
+    shaderBtnEl.classList.remove("held");
+    return false;
+  }
 });
 
 // on page load create a grid with a default 64x64 grid
